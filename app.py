c18483a39e581cfda87a18aafbcb9d13b08b03e2
satyam528 <satyam02410@gmail.com>
2026-04-04 11:35:49 +0530

    Image Uploading

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ st.title("MEDICAL BILL AUDITOR PLACEHOLDER")
 
 uploaded_file=st.file_uploader("PLEASE UPLOAD YOUR FILE HERE :",type=["pdf","jpeg","jpg","png"])
 
-if upload_file is not None:
+if uploaded_file is not None:
     st.write("FILE UPLOADED SUCCESSFULLY...")
 
+    #FILE INFO
     st.write("File Name : ", uploaded_file.name)
-    
+
+    #image
+    if uploaded_file.type.startswith("image"):
+        st.image(uploaded_file,caption="Uploaded Image",use_colomn_width= true ) 
+    else:
+        st.write("File uploaded,Preview will be added later")
