dd89cc0db0dd9a08d75234be5286afaa3ad9b21c
satyam528 <satyam02410@gmail.com>
2026-04-04 11:28:25 +0530

    UPDATION ON EXISTING UI

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ import streamlit as st
 st.set_page_config(page_title="MEDICAL-PLACEHOLDER")
 st.title("MEDICAL BILL AUDITOR PLACEHOLDER")
 
+uploaded_file=st.file_uploader("PLEASE UPLOAD YOUR FILE HERE :",type=["pdf","jpeg","jpg","png"])
+
+if upload_file is not None:
+    st.write("FILE UPLOADED SUCCESSFULLY...")
+
+    st.write("File Name : ", uploaded_file.name)
+    
