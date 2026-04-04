d7dc68978d4377ccf855d15a48718ffd270b1ce0
satyam528 <satyam02410@gmail.com>
2026-04-04 12:11:56 +0530

    OCR integration Done

--- app.py
+++ app.py
@@ -1,18 +1,36 @@
 import streamlit as st
+import easyocr
+from PIL import Image
+import numpy as np
 
 st.set_page_config(page_title="MEDICAL-PLACEHOLDER")
 st.title("MEDICAL BILL AUDITOR PLACEHOLDER")
 
 uploaded_file=st.file_uploader("PLEASE UPLOAD YOUR FILE HERE :",type=["pdf","jpeg","jpg","png"])
 
+
+
 if uploaded_file is not None:
-    st.write("FILE UPLOADED SUCCESSFULLY...")
+    st.success("FILE UPLOADED SUCCESSFULLY...")
 
     #FILE INFO
     st.write("File Name : ", uploaded_file.name)
 
+
+
+
     #image
     if uploaded_file.type.startswith("image"):
-        st.image(uploaded_file,caption="Uploaded Image",use_colomn_width= true ) 
+        image= Image.open(uploaded_file)
+        st.image(uploaded_file,caption="Uploaded Image",use_container_width= True ) 
+        
+        reader= easyocr.Reader(['en'])  
+        result= reader.readtext(np.array(image))
+
+        for item in result:
+            st.write(item[1])
+
+
+
     else:
         st.write("File uploaded,Preview will be added later")
